b6997c8d5562f16f2a9b0b90a7ea63a338727b7c
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-01-12 19:54:50 +0000

    Improve comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,4 @@
-// Array of special characters to be included in password
+// Array of special characters to be included in password.
 var specialCharacters = [
   '@',
   '%',
@@ -58,7 +58,7 @@ var lowerCasedCharacters = [
   'z'
 ];
 
-// Array of uppercase characters to be included in password
+// Array of uppercase characters to be included in password.
 var upperCasedCharacters = [
   'A',
   'B',
@@ -88,11 +88,11 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Function to prompt user for password options
+// Function to prompt user for password options.
 function getPasswordOptions() {
-  // Function used to remove repitition
+  // Function used to remove repitition.
   function confirmOption(name, characterSet, optionArray) {
-    // Check for user input to decide to include the character set or not
+    // Check for user input to decide to include the character set or not.
     if (confirm(`Use ${name} Characters? Press OK to use ${name} characters or cancel to not include them in the password.`))
       // Add the options to the array if the user chooses ok,
       return optionArray.push(...characterSet);
@@ -101,62 +101,63 @@ function getPasswordOptions() {
       return optionArray;
     };
   }
-  // Create the variables outside of the loops
+  // Create the variables outside of the loops.
   let characterNumber;
   let characterArray = [];
   while (true) {
     // An infinite loop is used until a correct number is input.
     characterNumber = parseInt(prompt("Choose a number of characters between 10 - 64."));
+    // Check the length is correct and the type is correct before moving on to the next step.
     if ((typeof characterNumber === "number") && (characterNumber >= 10) && (characterNumber <= 64)){
       break;
     }
     alert("The value input is not a numeric value between 10 - 64")
   }
   while (true) {
-    // Infinite loop that asks for each special character
+    // Infinite loop that asks for each special character.
     confirmOption("Uppercase", upperCasedCharacters, characterArray);
     confirmOption("Lowercase", lowerCasedCharacters, characterArray);
     confirmOption("Numeric", numericCharacters, characterArray);
     confirmOption("Special", specialCharacters, characterArray);
-    // Redo the loop if no options are chosen
+    // Redo the loop if no options are chosen.
     if (characterArray.length === 0) {
       continue;
     }
-    // Return the area that is the length of the password, with an array with all the possibilities in each position
+    // Return an array that is the length of the password, with an array with all the possibilities in each position of the array.
     return new Array(characterNumber).fill(characterArray)
   } 
 }
 
-// Function for getting a random element from an array
+// Function for getting a random element from an array.
 function getRandom(arr) {
-  // Get the length of the array and choose a number between the start and the end randomly
+  // Get the length of the array and choose a number between the start and the end randomly.
   return arr[Math.floor(Math.random()*arr.length)];
 }
 
-// Function to generate password with user input
+// Function to generate password with user input.
 function generatePassword() {
   // Create an empty string.
   let password = "";
   // Get an array the length of the password, with each one being all the possible characters in that position.
   let possibilities = getPasswordOptions();
-  // For every character choose one of the possibilities at random
+  // For every character choose one of the possibilities at random.
   for (i = 0; i < possibilities.length; i++) {
     password += (getRandom(possibilities[i]));
   }
-  // Return the constructed password
+  // Return the constructed password.
   return password;
 }
 
-// Get references to the #generate element
+// Get references to the #generate element.
 var generateBtn = document.querySelector('#generate');
 
-// Write password to the #password input
+// Write password to the #password input.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
-  // Set the password field on the page to the generated password
+  // Set the password field on the page to the generated password.
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+// Add event listener to generate button.
 generateBtn.addEventListener('click', writePassword);
